c3e0d26c8c6eea2543cff197c103c23fcb6656ab
Marcus Queiros <marcusdavi.soar@gmail.com>
2022-05-21 23:51:23 -0300

    getSpeciesByIds: Done

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // Seu código aqui
+function getSpeciesByIds(...ids) {
+  const res = [];
+  if (ids === undefined) return res;
+  ids.forEach((id) => res.push(data.species.find((specie) => specie.id === id)));
+  return res;
 }
 
 module.exports = getSpeciesByIds;
