3304f90c2908369bc327f79ae56c4b039de4392a
Marcus Queiros <marcusdavi.soar@gmail.com>
2022-05-23 18:26:12 -0300

    countAnimals: Done

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const animalsCounted = {};
+  data.species.forEach((spc) => { animalsCounted[spc.name] = spc.residents.length; });
+  if (animal === undefined) return animalsCounted;
+  if (animal.sex === undefined) return animalsCounted[animal.specie];
+  const specieFound = data.species.find((spc) => spc.name === animal.specie);
+  const filteredCounter = specieFound.residents.reduce((a, anm) => {
+    return anm.sex === animal.sex ? a += 1 : a;
+  }, 0)
+  return filteredCounter
 }
 
 module.exports = countAnimals;
