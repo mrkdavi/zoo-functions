682f59ee18d3cf1f89058b11a7cb8044d7df419d
Marcus Queiros <marcusdavi.soar@gmail.com>
2022-05-23 15:51:15 -0300

    getRelatedEmployees: Done

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) return {};
+  const { employees } = data;
+  const res = employees.find((emp) => (
+    emp.firstName === employeeName || emp.lastName === employeeName
+  ));
+  return res;
 }
 
 module.exports = getEmployeeByName;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,15 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const res = data.employees.some((emp) => emp.managers.some((mng) => mng === id));
+  return res;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  const managed = data.employees.filter((emp) => emp.managers.some((mng) => mng === managerId));
+  const managedFormatted = managed.map((v) => `${v.firstName} ${v.lastName}`);
+  return managedFormatted;
 }
 
 module.exports = { isManager, getRelatedEmployees };
