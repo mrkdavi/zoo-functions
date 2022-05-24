35a84d4b20b620f9ce8b499d52665c124b48d27f
Marcus Queiros <marcusdavi.soar@gmail.com>
2022-05-24 16:14:25 -0300

    getOldestFromFirstSpecies: Done

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,28 @@
 const data = require('../data/zoo_data');
 
+function getEmployeeById(id) {
+  const employee = data.employees.find((emp) => emp.id === id);
+  return employee;
+}
+
+function getSpecieById(id) {
+  const specie = data.species.find((spc) => spc.id === id);
+  return specie;
+}
+
+function getFirstSpecieFromEmployee(id) {
+  const employee = getEmployeeById(id);
+  const firstSpecieId = employee.responsibleFor[0];
+  const specie = getSpecieById(firstSpecieId);
+  return specie;
+}
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const { residents } = getFirstSpecieFromEmployee(id);
+  residents.sort((a, b) => b.age - a.age);
+  const oldestAnimal = residents[0];
+  const oldestAnimalFormatted = Object.values(oldestAnimal);
+  return oldestAnimalFormatted;
 }
 
 module.exports = getOldestFromFirstSpecies;
