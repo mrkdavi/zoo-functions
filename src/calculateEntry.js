cf9a4e72c21c42e0a8fc4e69fc757b51a33bef06
Marcus Queiros <marcusdavi.soar@gmail.com>
2022-05-23 23:18:02 -0300

    calculateEntry: Done

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,28 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const entrantsCounted = {
+    child: 0,
+    adult: 0,
+    senior: 0,
+  };
+  entrants.forEach((entrant) => {
+    if (entrant.age < 18) entrantsCounted.child += 1;
+    else if (entrant.age < 50) entrantsCounted.adult += 1;
+    else if (entrant.age >= 50) entrantsCounted.senior += 1;
+  });
+  return entrantsCounted;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined) return 0;
+  if (Object.entries(entrants).length === 0) return 0;
+  if (entrants === 0) return 1;
+  const entryValues = Object.entries(countEntrants(entrants)).reduce((acc, v, i) => {
+    const sum = acc + (v[1] * data.prices[v[0]]);
+    return sum;
+  }, 0);
+  return entryValues;
 }
 
 module.exports = { calculateEntry, countEntrants };
