40a7609711888236f3a962a63056eae67bfb1fe9
Marcus Queiros <marcusdavi.soar@gmail.com>
2022-05-25 09:43:01 -0300

    getOpeningHours.test: Done

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,58 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function getEmployeeById(id) {
+  const employee = data.employees.find((emp) => emp.id === id);
+  if (employee === undefined) throw new Error('Informações inválidas');
+  return employee;
+}
+
+function getAnimalById(id) {
+  const animal = data.species.find((spc) => spc.id === id);
+  if (animal === undefined) throw new Error('Informações inválidas');
+  return animal;
+}
+
+function getEmployeeByName(name) {
+  const employee = data.employees.find((emp) => [emp.firstName, emp.lastName].includes(name));
+  return employee;
+}
+
+function employeeFormatter(id, fullName, speciesId) {
+  const species = speciesId.map((specieId) => getAnimalById(specieId));
+  const speciesName = species.map((spc) => spc.name);
+  const speciesLocation = species.map((spc) => spc.location);
+  const employee = {
+    id,
+    fullName,
+    species: speciesName,
+    locations: speciesLocation,
+  };
+  return employee;
+}
+
+function getEmployeeFormatted(employee) {
+  const { id, responsibleFor } = employee;
+  const fullName = `${employee.firstName} ${employee.lastName}`;
+  const employeeFormatted = employeeFormatter(id, fullName, responsibleFor);
+  return employeeFormatted;
+}
+
+function getAllEmployeesFormatted() {
+  const { employees } = data;
+  const employeesFormatted = employees.map((emp) => getEmployeeFormatted(emp));
+  return employeesFormatted;
+}
+
+function getEmployeesCoverage(parm) {
+  const parmKey = parm !== undefined ? Object.keys(parm)[0] : undefined;
+  switch (parmKey) {
+  case 'id':
+    return getEmployeeFormatted(getEmployeeById(parm.id));
+  case 'name':
+    return getEmployeeFormatted(getEmployeeByName(parm.name));
+  default:
+    return getAllEmployeesFormatted();
+  }
 }
 
 module.exports = getEmployeesCoverage;
--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,55 @@
 const getOpeningHours = require('../src/getOpeningHours');
+const { hours } = require('../data/zoo_data');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  it('A função getOpeningHours deve retornar todos os horários quando não passado nenhum parâmetro', () => {
+    const actual = getOpeningHours();
+    const expected = hours;
+    expect(actual).toEqual(expected);
+  });
+  it('A função getOpeningHours deve retornar uma string quando passado os parâmetros corretos', () => {
+    const actual = getOpeningHours('Monday', '09:00-AM');
+    const expected = 'The zoo is closed';
+    expect(actual).toMatch(expected);
+  });
+  it('A função getOpeningHours deve retornar uma string quando passado os parâmetros corretos', () => {
+    const actual = getOpeningHours('Tuesday', '09:00-AM');
+    const expected = 'The zoo is open';
+    expect(actual).toMatch(expected);
+  });
+  it('A função getOpeningHours deve retornar uma string quando passado os parâmetros corretos', () => {
+    const actual = getOpeningHours('Wednesday', '09:00-PM');
+    const expected = 'The zoo is close';
+    expect(actual).toMatch(expected);
+  });
+  it('A função getOpeningHours deve retornar um erro quando passado os parâmetros errados', () => {
+    const actual = () => getOpeningHours('Wednesday', '13:00-AM');
+    const expected = 'The hour must be between 0 and 12';
+    expect(actual).toThrowError(expected);
+  });
+  it('A função getOpeningHours deve retornar um erro quando passado os parâmetros errados', () => {
+    const actual = () => getOpeningHours('Wednesday', '10:93-AM');
+    const expected = 'The minutes must be between 0 and 59';
+    expect(actual).toThrowError(expected);
+  });
+  it('A função getOpeningHours deve retornar um erro quando passado os parâmetros errados', () => {
+    const actual = () => getOpeningHours('Wednesday', '10:23-FM');
+    const expected = 'The abbreviation must be \'AM\' or \'PM\'';
+    expect(actual).toThrowError(expected);
+  });
+  it('A função getOpeningHours deve retornar um erro quando passado os parâmetros errados', () => {
+    const actual = () => getOpeningHours('ChuchuBeleza', '10:23-FM');
+    const expected = 'The day must be valid. Example: Monday';;
+    expect(actual).toThrowError(expected);
+  });
+  it('A função getOpeningHours deve retornar um erro quando passado os parâmetros errados', () => {
+    const actual = () => getOpeningHours('Sunday', 'nine:11-AM');
+    const expected = `The hour should represent a number`;;
+    expect(actual).toThrowError(expected);
+  });
+  it('A função getOpeningHours deve retornar um erro quando passado os parâmetros errados', () => {
+    const actual = () => getOpeningHours('Sunday', '09:eleven-AM');
+    const expected = `The minutes should represent a number`;;
+    expect(actual).toThrowError(expected);
+  });
+});
